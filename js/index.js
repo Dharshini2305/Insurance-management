410449591d29af08df7efd65a64ccc4c96b6f333
Dharshini <dharshini0523@gmail.com>
2025-05-13 10:06:40 +0530

    hello world

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const reset=document.getElementById("reset");
+reset.addEventListener("click",function(event){
+    event.preventDefault()
+    const email=document.getElementById("email").value;
+})
